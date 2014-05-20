12ea4d47a659d6197e385b0a37064f9f2c1dff9f
Dmitry Savin <savin77@gmail.com>
2014-05-20 13:35:54 +0400

    Рабочий переход по дереву с изменением форм

--- allocjpa/src/com/spc/forms/ClassifForm.java
+++ allocjpa/src/com/spc/forms/ClassifForm.java
@@ -8,6 +8,7 @@ import java_cup.internal_error;
 import com.vaadin.event.ItemClickEvent;
 import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.server.Page;
+import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Notification;
@@ -18,68 +19,44 @@ public class ClassifForm extends HorizontalSplitPanel {
 	private static final long serialVersionUID = 8171549744161227310L;
 
 	private Tree navigationTree;
-	private FrmPersons frmPersons;
-	private FrmAdresses frmAdresses;
 
-	private String treeItemClassif = "Справочники";
-	private String treeItemClassifPerson = "Person";
-	private String treeItemClassifAdress = "Adress";
-
-	// Пункт меню (ID), Форма, Родитель (ID Родителя)
+	// Пункт меню { (ID), Форма, Родитель (ID Родителя) }
 	private static Object[][] treeItems = new Object[][] {
 		new Object[] { "Заголовочный пункт дерева" },
 		new Object[] { "Справочнички"},
 				 new Object[] {"Persons", "Справочнички", new FrmPersons()},
 				 new Object[] {"Адреса","Справочнички", new FrmAdresses()},
+				 new Object[] {"Добавляемые Persons", "Справочнички", new FrmPersonsEdit()},
 		new Object[] { "Завершающий пукт дерева" } };
 
-	//private HashMap<String, Object> treeItemsFrms;
 	private HashMap treeItemsFrms = new HashMap();
-	
-	private Object[] treeItemsForms = new Object[] {
-			"Persons", new FrmPersons()
-	};
+
 
 	public ClassifForm() {
 		super();
 		buildForm();
 		addListener();
-		// setSecondComponent(frmPersons);
 	}
 
 	private void buildForm() {
 		navigationTree = new Tree();
 		navigationTree.setCaption("Классификаторы");
-		navigationTree.addItem(treeItemClassif);
-		navigationTree.setChildrenAllowed(treeItemClassif, true);
-
-		navigationTree.addItem(treeItemClassifPerson);
-		navigationTree.setParent(treeItemClassifPerson, treeItemClassif);
-
-		navigationTree.addItem(treeItemClassifAdress);
-		navigationTree.setParent(treeItemClassifAdress, treeItemClassif);
-
-		navigationTree.expandItemsRecursively(treeItemClassif);
-		navigationTree.setNullSelectionAllowed(false);
-
 		setFirstComponent(navigationTree);
 		setSplitPosition(200, Unit.PIXELS);
 
-		frmPersons = new FrmPersons();
-		frmAdresses = new FrmAdresses();
-
 		for(int i=0; i < treeItems.length; i++) {
 			System.out.println(treeItems[i][0]);
-			navigationTree.addItem(treeItems[i][0]);
+			navigationTree.addItem((String) treeItems[i][0]);
 			System.out.println(treeItems[i].length);
 			if( treeItems[i].length > 1) {
+					navigationTree.setChildrenAllowed((String) treeItems[i][1], true);
 					String currItemString = (String) treeItems[i][0];
-					System.out.println("Установим родителя " + treeItems[i][1]);
-					navigationTree.setParent(treeItems[i][0], treeItems[i][1]);
-					System.out.println("Добавим связь формы и ид части дерева для " + treeItems[i][0] + " форма " + treeItems[i][2]);
+					navigationTree.setParent((String) treeItems[i][0], (String) treeItems[i][1]);
+					navigationTree.setChildrenAllowed((String) treeItems[i][0], false);
 					treeItemsFrms.put((String) treeItems[i][0], treeItems[i][2] );
 			} else {
 				navigationTree.setChildrenAllowed(treeItems[i][0], false);
+				System.out.println(navigationTree);
 			}
 		}
 	}
@@ -89,18 +66,12 @@ public class ClassifForm extends HorizontalSplitPanel {
 			private static final long serialVersionUID = 1L;
 
 			public void itemClick(ItemClickEvent event) {
-				// TODO Auto-generated method stub
 				Object value = event.getItemId();
 				Notification.show((String) value);
-				if (treeItemClassifPerson.equals(value)) {
-
-					setSecondComponent(frmPersons);
-				} else if (treeItemClassifAdress.equals(value)) {
-					Notification.show("Отработал elseif");
-					setSecondComponent(frmAdresses);
-				}
-				navigationTree.select(event);
-				System.out.println("ttt");
+				AbstractLayout secondComponent;
+				secondComponent = (AbstractLayout) treeItemsFrms.get(value);
+				setSecondComponent(secondComponent);
+				System.out.println("selected tree itemid: "+(String) value);
 			}
 		});
 	}
